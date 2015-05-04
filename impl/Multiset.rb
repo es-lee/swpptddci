ecfcb2bb93131f605551f3b852631143ea545729
es-lee <dldms800@gmail.com>
2015-05-04 20:39:24 +0900

    multiset

--- impl/Multiset.rb
+++ impl/Multiset.rb
@@ -1,1 +1,24 @@
+class Multiset
+  def initialize(n)
+    @set = [n]
+  end
 
+  def contains(n)
+    return @set.include?(n)
+  end
+
+  def add(n)
+    @set.insert(0, n)
+  end
+
+  def remove(n)
+    if self.contains(n)
+      for i in 0..@set.length
+        if @set[i] == n
+          @set.delete_at(i)
+          break
+        end
+      end
+    end
+  end
+end
